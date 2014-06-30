49a2efdc2fd9e08759f4568e35c517a5f1cef9df
Redha Gouicem <redha.gouicem@gmail.com>
2014-06-30 15:21:12 +0200

    Added arch-independant packages support + some refactoring + doc

--- scripts/config_parser.pl
+++ scripts/config_parser.pl
@@ -44,16 +44,26 @@ $config =~ s#="//#://#g;
 $config =~ s/\n{2,}/\n/g;
 $config =~ s/^\n(.+)$/$1/sg;
 
-#Adding comas in BUILDEPEND and PACKAGEDEPENDS
+#Adding comas in BUILDEPENDS, BINRUNDEPENDS, LIBRUNDEPENDS, INDRUNDEPENDS
 $config =~ /BUILDDEPENDS=\"(.*?)\";/;
 $res=$1;
 $res=~s/ (?=[^\d\(\s])/, /g, $1;
 $config =~ s/BUILDDEPENDS=\"(.*?)\";/BUILDDEPENDS=\"$res\";/;
 
-$config =~ /PACKAGEDEPENDS=\"(.*?)\";/;
+$config =~ /BINRUNDEPENDS=\"(.*?)\";/;
 $res=$1;
 $res=~s/ (?=[^\d\(\s])/, /g, $1;
-$config =~ s/PACKAGEDEPENDS=\"(.*?)\";/PACKAGEDEPENDS=\"$res\";/;
+$config =~ s/BINRUNDEPENDS=\"(.*?)\";/BINRUNDEPENDS=\"$res\";/;
+
+$config =~ /LIBRUNDEPENDS=\"(.*?)\";/;
+$res=$1;
+$res=~s/ (?=[^\d\(\s])/, /g, $1;
+$config =~ s/LIBRUNDEPENDS=\"(.*?)\";/LIBRUNDEPENDS=\"$res\";/;
+
+$config =~ /INDRUNDEPENDS=\"(.*?)\";/;
+$res=$1;
+$res=~s/ (?=[^\d\(\s])/, /g, $1;
+$config =~ s/INDRUNDEPENDS=\"(.*?)\";/INDRUNDEPENDS=\"$res\";/;
 
 #Checking version
 $config =~ /VERSION=\"(.*?)\";/;
@@ -63,15 +73,24 @@ exit 4 if ($1 eq "");
 $config =~ /PACKAGETYPE=\"(.*?)\";/;
 exit 2 if ($1 eq "");
 my@types=split / /, $1;
-exit 3 if ($types[0] ne "s" && $types[0] ne "l");
-exit 3 if (($types[1] ne "") && ($types[1] ne "s") && ($types[1] ne "l"));
+#exit 3 if ($types[0] ne "s" && $types[0] ne "l");
+#exit 3 if (($types[1] ne "") && ($types[1] ne "s") && ($types[1] ne "l"));
+foreach my$t (@types) {
+    exit 3 if ($t ne "s" && $t ne "l" && $t ne "i");
+}
 
-#Checking BINPACKAGENAME and LIBPACKAGENAME
+#Checking BINPACKAGENAME, LIBPACKAGENAME and INDPACKAGENAME
 $config=~/BINPACKAGENAME=\"(.*?)\";/;
-exit 5 if ($1 eq "" && ($types[0] eq "s" || $types[1] eq "s"));
+exit 5 if ($1 eq "" && ($types[0] eq "s" || $types[1] eq "s" 
+			|| $types[2] eq "s"));
 
 $config=~/LIBPACKAGENAME=\"(.*?)\";/;
-exit 6 if ($1 eq "" && ($types[0] eq "l" || $types[1] eq "l"));
+exit 6 if ($1 eq "" && ($types[0] eq "l" || $types[1] eq "l"
+			|| $types[2] eq "l"));
+
+$config=~/INDPACKAGENAME=\"(.*?)\";/;
+exit 11 if ($1 eq "" && ($types[0] eq "i" || $types[1] eq "i"
+			|| $types[2] eq "i"));
 
 #Checking COPYRIGHT and DEVS variables
 $config =~ /DEVS=\"(.*?)\";/;
@@ -123,14 +142,22 @@ if ($isvalue==1) {
 #Checking if BINARYNAMES is empty
 $config =~ /BINARYNAMES=\"(.*?)\";/;
 my$bins=$1;
-exit 9 if ($bins eq "" && ($types[0] eq "s" || $types[1] eq "s"));
+exit 9 if ($bins eq "" && ($types[0] eq "s" || $types[1] eq "s"
+			   || $types[2] eq "s"));
 
 #Checking if LIBNAMES or HEADERNAMES is empty
 $config =~ /LIBNAMES=\"(.*?)\";/;
 my$libs=$1;
 $config =~ /HEADERNAMES=\"(.*?)\";/;
 my$headers=$1;
-exit 10 if (($types[0] eq "l" || $types[1] eq "l") && $libs eq "" 
-	    && $headers eq "");
+exit 10 if (($types[0] eq "l" || $types[1] eq "l" || $types[2] eq "l")
+	    && $libs eq "" && $headers eq "");
+
+#Checking if INDNAMES is empty
+$config =~ /INDNAMES=\"(.*?)\";/;
+my$inds=$1;
+exit 12 if (($types[0] eq "i" || $types[1] eq "i" || $types[2] eq "i")
+	    && $inds eq "");
+
 print $config;
 close IN;
